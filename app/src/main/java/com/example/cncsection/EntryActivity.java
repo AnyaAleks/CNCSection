7efb1af59274e6c54f799b7a0e07c88446bfc86d
AnyaAleks <99331005+AnyaAleks@users.noreply.github.com>
2024-09-19 14:38:07 +0300

    create base activity

---
+++ app/src/main/java/com/example/cncsection/EntryActivity.java
@@ -0,0 +1,33 @@
+package com.example.cncsection;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_entry);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void goNext(View V){
+        //Сделать проверку на пароль и понять какое окно грузить по типу клиента
+        //пока стоит заглушка
+        Intent intent = new Intent(this, CreateOrderActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/cncsection/MainActivity.java
+++ app/src/main/java/com/example/cncsection/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.cncsection;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
@@ -23,38 +25,53 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        myDBHlpr = new DataBaseHelper(this);
+        Intent intent = new Intent(this, EntryActivity.class);
 
-        Cursor csr = myDBHlpr.getAll("Machine");
-        while(csr.moveToNext()){
-            Log.d("DB_ROWINFO",
-                    "ID is " + csr.getString(csr.getColumnIndex("id_machine"))
-                            + " TYPE is " + csr.getString((csr.getColumnIndex("type"))
-                    )
-            );
-        }
+        CountDownTimer timer = new CountDownTimer(3000, 1000)
+        {
+            public void onTick(long l){}
 
-        csr = myDBHlpr.sortInOrder("Request", "production_time");
-        while(csr.moveToNext()){
-            Log.d("SORT_REQUEST",
-                    "ID_REC is " + csr.getString(csr.getColumnIndex("id_request"))
-                            + " ID_PART is " + csr.getString(csr.getColumnIndex("id_part"))
-                            + " TIME is " + csr.getString((csr.getColumnIndex("production_time"))
-                    )
-            );
-        }
-
-        csr = myDBHlpr.searchItem("Osnaska", "id_osnaska", "2");
-        while(csr.moveToNext()){
-            Log.d("SEARCH",
-                    "ID_OSN is " + csr.getString(csr.getColumnIndex("id_osnaska"))
-                            + " TITLE is " + csr.getString(csr.getColumnIndex("title"))
-                            + " STOCK is " + csr.getString((csr.getColumnIndex("stock_availability"))
-                    )
-            );
-        }
+            @Override
+            public void onFinish()
+            {
+                startActivity(intent);
+            };
+        }.start();
 
 
-        csr.close();
+
+//        myDBHlpr = new DataBaseHelper(this);
+//
+//        Cursor csr = myDBHlpr.getAll("Machine");
+//        while(csr.moveToNext()){
+//            Log.d("DB_ROWINFO",
+//                    "ID is " + csr.getString(csr.getColumnIndex("id_machine"))
+//                            + " TYPE is " + csr.getString((csr.getColumnIndex("type"))
+//                    )
+//            );
+//        }
+//
+//        csr = myDBHlpr.sortInOrder("Request", "production_time");
+//        while(csr.moveToNext()){
+//            Log.d("SORT_REQUEST",
+//                    "ID_REC is " + csr.getString(csr.getColumnIndex("id_request"))
+//                            + " ID_PART is " + csr.getString(csr.getColumnIndex("id_part"))
+//                            + " TIME is " + csr.getString((csr.getColumnIndex("production_time"))
+//                    )
+//            );
+//        }
+//
+//        csr = myDBHlpr.searchItem("Osnaska", "id_osnaska", "2");
+//        while(csr.moveToNext()){
+//            Log.d("SEARCH",
+//                    "ID_OSN is " + csr.getString(csr.getColumnIndex("id_osnaska"))
+//                            + " TITLE is " + csr.getString(csr.getColumnIndex("title"))
+//                            + " STOCK is " + csr.getString((csr.getColumnIndex("stock_availability"))
+//                    )
+//            );
+//        }
+//
+//
+//        csr.close();
     }
 }
