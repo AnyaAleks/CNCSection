93fc4a3356cf929fcb05809b3037d91d54bbc7fe
Arina0000000 <arinag23@mail.ru>
2024-09-23 09:17:13 +0300

    Сделан интерфейс входа в систему (без проверки пароля и переадресовки). Также сделан интерфейс оформление заявки и создана конструкция (делегат) для просмотра статуса заявки.

--- app/src/main/java/com/example/cncsection/CreateOrderActivity.java
+++ app/src/main/java/com/example/cncsection/CreateOrderActivity.java
@@ -42,6 +42,7 @@ public class CreateOrderActivity extends AppCompatActivity {
                 String coment = com.getText().toString();
             }
         });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
