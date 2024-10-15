934a7cc114a36042b4b7c7e10687425d4e890be1
Arimlock <aristotelleonov@gmail.com>
2024-10-15 14:09:42 +0300

    Fix execute header
    We delete "EdgeToEdge.enable(this);" in Execute...java

--- app/src/main/java/com/example/cncsection/ExecuteOrderActivity.java
+++ app/src/main/java/com/example/cncsection/ExecuteOrderActivity.java
@@ -24,7 +24,7 @@ public class ExecuteOrderActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_execute_order);
 
         order_status_spinner = (Spinner) findViewById(R.id.order_status_spinner);
