5dcc7f73ebe8f97caac3998d16ae16e9f26db3d5
Arina0000000 <arinag23@mail.ru>
2024-10-15 22:11:09 +0300

    Added password reset and hiding, but no eye to see the entered password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,6 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
-
     //implementation("com.github.barteksc:android-pdf-viewer:1.4.0")
     //implementation("com.joanzapata.pdfview:android-pdfview:1.0.4@aar")
 
--- app/src/main/java/com/example/cncsection/EntryActivity.java
+++ app/src/main/java/com/example/cncsection/EntryActivity.java
@@ -4,12 +4,14 @@ import static android.app.PendingIntent.getActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,6 +46,7 @@ public class EntryActivity extends AppCompatActivity {
         password_1 = findViewById(R.id.password);
         login = findViewById(R.id.login);
         add_button = findViewById(R.id.add_button);
+
         //add_button.setOnClickListener(new View.OnClickListener() {
         //@Override
         //public void onClick(View view) {
@@ -53,36 +56,51 @@ public class EntryActivity extends AppCompatActivity {
         //});
 
     }
+
     @SuppressLint("Range")
-    public void goNext(View V){
+    public void goNext(View V) {
         //Сделать проверку на пароль и понять какое окно грузить
         // (всем работникам при устройстве на работу должны выдавать пароль) по типу клиента
         //У каждой роли в пароле можно сделать определённые первые цифры или буквы и делать проверку по ним
         //Toast.makeText(getBaseContext(), "Reason can not be blank", Toast.LENGTH_SHORT).show();
         String passwordMD5 = md5(password_1.getText().toString());
+        boolean isValid = false;
 
         Cursor csr = dbStaff.getAll("Staff");
-        while(csr.moveToNext()){
+        while (csr.moveToNext()) {
             //Если Логин и Пароль есть в БД
             Log.d("DB_STAFF", csr.getString(csr.getColumnIndex("fio")));
-            if(login.getText().toString().equals(csr.getString(csr.getColumnIndex("fio")).toString())
+            if (login.getText().toString().equals(csr.getString(csr.getColumnIndex("fio")).toString())
                     && passwordMD5.equals(csr.getString(csr.getColumnIndex("password")).toString())
-            ){
+            ) {
+                isValid = true;
                 //Поиск роли
                 Cursor csrAccess = dbStaff.getAll("Access");
-                while(csrAccess.moveToNext()){
-                    if(csr.getInt(csr.getColumnIndex("id_access")) ==
-                            csrAccess.getInt(csrAccess.getColumnIndex("id_access"))){
+                while (csrAccess.moveToNext()) {
+                    if (csr.getInt(csr.getColumnIndex("id_access")) ==
+                            csrAccess.getInt(csrAccess.getColumnIndex("id_access"))) {
                         Log.d("DB_STAFF2", csr.getInt(csr.getColumnIndex("id_access"))
                                 + " " + csrAccess.getInt(csrAccess.getColumnIndex("id_access")));
                         openActivityByAccess(csr.getInt(csrAccess.getColumnIndex("id_access")));
                     }
 
                 }
-            } else {
+            }
+            break;
+        }
+        if (!isValid) {
+            login.setText("");
+            password_1.setText("");
+            TextView errorTextView = findViewById(R.id.error_text_view);
+            errorTextView.setText("Логин или пароль введены неправильно");
+        }
+    }
+
+
+            // else {
 //                Toast toast2 = Toast.makeText(this, "Пользователь не найден", Toast.LENGTH_SHORT);
 //                toast2.show();
-            }
+ //           }
 
 //            Log.d("DB_STAFF",
 //                    "ID = " + csr.getInt(csr.getColumnIndex("id_staff"))
@@ -91,8 +109,7 @@ public class EntryActivity extends AppCompatActivity {
 //                            + " PASSWORD = " + csr.getString(csr.getColumnIndex("password"))
 //            );
 
-        }
-    }
+
 
     public String md5(String s) {
         try {
