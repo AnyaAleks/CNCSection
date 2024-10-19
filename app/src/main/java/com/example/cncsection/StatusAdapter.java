8b265f0b4d8dc61ff65fa546ec2d4305b2057c93
Arina0000000 <arinag23@mail.ru>
2024-10-19 23:58:16 +0300

    Added SearchView in Manager

--- app/src/main/java/com/example/cncsection/StatusAdapter.java
+++ app/src/main/java/com/example/cncsection/StatusAdapter.java
@@ -1,43 +1,54 @@
 package com.example.cncsection;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class StatusAdapter extends RecyclerView.Adapter<StatusAdapter.ViewHolder>{
+public class StatusAdapter extends RecyclerView.Adapter<StatusAdapter.ViewHolder> {
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView applicationView;
-        public TextView statusView;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-
-            applicationView = (TextView) itemView.findViewById(R.id.input_number);
-            statusView = (TextView) itemView.findViewById(R.id.input_status);
-        }
-    }
     private List<Status> statuses;
 
     public StatusAdapter(List<Status> st) {
         statuses = st;
     }
 
+    // method for filtering our recyclerview items.
+    @SuppressLint("NotifyDataSetChanged")
+    public void filterList(ArrayList<Status> filterList) {
+        statuses = filterList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
     @Override
     public StatusAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         View contactView = inflater.inflate(R.layout.delegate_orderlist, parent, false);
-        ViewHolder viewHolder = new ViewHolder(contactView);
-        return viewHolder;
+        return new ViewHolder(contactView);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView applicationView;
+        public TextView statusView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            applicationView = (TextView) itemView.findViewById(R.id.input_number);
+            statusView = (TextView) itemView.findViewById(R.id.input_status);
+        }
     }
 
     @Override
