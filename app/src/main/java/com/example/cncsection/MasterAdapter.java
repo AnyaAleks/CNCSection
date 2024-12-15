6cb1338e764bf82d8adcbd80221ddab364fb6c30
4315 Леонов А.Я. <aristotelleonov@gmail.com>
2024-12-16 02:12:26 +0300

    Umenshaet pri udalenii u mastera

--- app/src/main/java/com/example/cncsection/MasterAdapter.java
+++ app/src/main/java/com/example/cncsection/MasterAdapter.java
@@ -3,9 +3,11 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -39,16 +41,29 @@ public class MasterAdapter extends ArrayAdapter<MasterString> {
         //itemText.setText(currentNumberPosition.getName());
 
         ImageButton deleteImage = currentItemView.findViewById(R.id.delete_button);
+        View finalCurrentItemView = currentItemView;
         deleteImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MasterString currentNumberPosition = getItem(position);
                 remove(currentNumberPosition);
                 notifyDataSetChanged();
+
+                ChangeHeight(finalCurrentItemView);
             }
         });
 
         // then return the recyclable view
         return currentItemView;
+    }
+    private void ChangeHeight(View view)
+    {
+        ViewGroup parent = (ViewGroup) view.getParent();
+        int totalHeight = parent.getHeight();
+
+        ViewGroup.LayoutParams params = parent.getLayoutParams();
+        params.height = totalHeight - view.getHeight();
+        parent.setLayoutParams(params);
+        parent.requestLayout();
     }
 }
